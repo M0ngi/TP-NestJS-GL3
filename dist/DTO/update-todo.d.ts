cc1b8517bbb114a36cc6f925152d71e3edf150ed
M0ngi <saidanemongi@gmail.com>
2023-04-01 14:12:09 +0100

    remove /dist & add to ignore

--- dist/DTO/update-todo.d.ts
+++
@@ -1,8 +0,0 @@
-import { TodoStatusEnum } from "src/todo/todo";
-import { CreateTodoDto } from "./create-todo";
-declare const UpdateTodoTdo_base: import("@nestjs/mapped-types").MappedType<Partial<CreateTodoDto>>;
-export declare class UpdateTodoTdo extends UpdateTodoTdo_base {
-    id: string;
-    status?: TodoStatusEnum;
-}
-export {};
